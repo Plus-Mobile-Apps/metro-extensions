9dbdd18a82c9ecb668badd40cfac1dab1630f2c4
Andrew Steinmetz <andrew@plusmobileapps.com>
2026-05-16 22:50:00 -0700

    feat: enable publishing to maven central for plusmobileapps

--- lib/assisted-factory/runtime/build.gradle.kts
+++ lib/assisted-factory/runtime/build.gradle.kts
@@ -1,9 +1,11 @@
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.mavenPublish)
 }
 
 kotlin {
@@ -43,4 +45,46 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+}
+
+mavenPublishing {
+    coordinates(
+        "com.plusmobileapps.metro-extensions",
+        "assisted-factory-runtime",
+        libs.versions.assistedFactory.get(),
+    )
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
+
+    pom {
+        name.set("Metro Extensions - Assisted Factory Runtime")
+        description.set("Runtime marker annotations for the Metro assisted-factory KSP extension.")
+        inceptionYear.set("2026")
+        url.set("https://github.com/plusmobileapps/metro-extensions/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("plusmobileapps")
+                name.set("Plus Mobile Apps LLC")
+                url.set("https://github.com/plusmobileapps/")
+            }
+        }
+        scm {
+            url.set("https://github.com/plusmobileapps/metro-extensions/")
+            connection.set("scm:git:git://github.com/plusmobileapps/metro-extensions.git")
+            developerConnection.set("scm:git:ssh://git@github.com/plusmobileapps/metro-extensions.git")
+        }
+    }
+}
+
+val signingTasks = tasks.withType<Sign>()
+tasks.withType<AbstractPublishToMaven>().configureEach {
+    dependsOn(signingTasks)
 }
